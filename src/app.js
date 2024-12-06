260f6df6769dc99c4afcd211cd7fd4a8e4823e29
Vitaly Gomer <168081524+VgomerV@users.noreply.github.com>
2024-12-06 16:12:32 +0300

    Update app.js

--- src/app.js
+++ src/app.js
@@ -189,7 +189,7 @@ export default () => {
       .catch(() => {
         watchedState.form.isValid = false;
         watchedState.form.processState = 'error';
-        watchedState.feedbackMessage = i18n.t('errors.notRss');
+        watchedState.feedbackMessage = i18n.t('errors.notValid');
       });
   });
 };
