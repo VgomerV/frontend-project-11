8809a05d8ebfbb5b68026e09f5255a686a1803f1
Vitaly Gomer <168081524+VgomerV@users.noreply.github.com>
2024-12-12 16:10:54 +0300

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ import path from 'path';
 import HtmlWebpackPlugin from 'html-webpack-plugin';
 
 export default {
-  mode: 'production',
+  mode: 'development',
   entry: {
     filename: path.resolve(process.cwd(), 'src/index.js'),
   },
